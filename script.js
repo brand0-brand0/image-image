ebd3c352ea2d183cb68d843cb91f71b328559772
brand0-brand0 <161082580+brand0-brand0@users.noreply.github.com>
2024-02-24 13:56:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const slide = document.querySelector('.carouselSlide');
-    let speed = 1; // Adjust speed as necessary
-    let shiftAmount = 0; // Track the total amount shifted
-
-    // Clone the entire slide for a seamless loop
-    const clone = slide.cloneNode(true);
-    slide.appendChild(clone);
-
-    function moveCarousel() {
-        shiftAmount += speed;
-        if (shiftAmount > slide.offsetWidth) {
-            shiftAmount = 0; // Reset shift amount when it exceeds half the width
-        }
-        slide.style.transform = `translateX(-${shiftAmount}px)`;
-
-        requestAnimationFrame(moveCarousel);
-    }
-
-    moveCarousel();
+    const carouselSlide = document.querySelector('.carouselSlide');
+    const images = document.querySelectorAll('.carouselSlide img');
+    
+    // Clone each image and append to the carousel for a seamless loop
+    images.forEach(image => {
+        const clone = image.cloneNode(true);
+        carouselSlide.appendChild(clone);
+    });
 });
